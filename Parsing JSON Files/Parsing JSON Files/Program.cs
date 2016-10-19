c93134fb46b4cf3e493a2ac6a2676ca7407b9a96
Oday <Oday.9@hotmail.com>
2016-10-19 18:48:22 +0300

    Initial

---
+++ Parsing JSON Files/Parsing JSON Files/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Include
+using System.IO;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+
+namespace Parsing_JSON_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string jsonText = File.ReadAllText("Student.json");
+            //JavaScriptSerializer serializer = new JavaScriptSerializer();
+            //Student student = serializer.Deserialize<Student>(jsonText);
+            //Console.WriteLine(student.Name + ":" + student.Age);
+            //Console.ReadKey();
+
+            //Josn.net
+            string jsonText = File.ReadAllText("Student.json");
+            Student student = JsonConvert.DeserializeObject<Student>(jsonText);
+            Console.WriteLine(student.Name + ":" + student.Age);
+            Console.ReadKey();
+        }
+    }
+}
